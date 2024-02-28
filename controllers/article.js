913c11342340d7836651b1c388f10fdbec52fc47
Victoria <mariavictoriaballesp@gmail.com>
2024-02-28 14:43:56 -0400

    feat(controller,routes): new routes with new methods added, such as searcher and upload image.

--- controllers/article.js
+++ controllers/article.js
@@ -1,5 +1,7 @@
 const Article = require("../models/Article");
 const validator = require("validator");
+const fs = require("fs");
+const path = require("path");
 
 //Creamos todos los métodos
 
@@ -112,15 +114,24 @@ const updateArticle = (req, res) => {
   let id = req.params.id;
   let changedArticle = req.body;
   let lenghtOfArticle = 0;
-  let validatedTitle = validator.isEmpty(changedArticle.title);
-  let validatedBody = validator.isEmpty(changedArticle.body);
+  let validatedTitle;
+  let validatedBody;
 
-  for (let key in changedArticle) {
-    lenghtOfArticle++;
+  if (req.body.title == undefined && req.body.body != undefined) {
+    validatedTitle = true;
+    validatedBody = validator.isEmpty(req.body.body);
+  } else if (req.body.title != undefined && req.body.body == undefined) {
+    validatedBody = true;
+    validatedTitle = validator.isEmpty(req.body.title);
+  } else if (req.body.title != undefined && req.body.body != undefined) {
+    return res.status(400).json({
+      status: "error",
+      mensaje: "Request is empty or incomplete.",
+    });
   }
 
   try {
-    if (validatedTitle || validatedBody) {
+    if (validatedTitle && validatedBody) {
       throw new error("Request of validation denegaded.");
     }
   } catch (error) {
@@ -147,27 +158,144 @@ const updateArticle = (req, res) => {
           mensaje: "An error ocurred with the updating proccess.",
         });
       });
-  } else if (!validatedBody || !validatedTitle) {
-    if (!validatedTitle) {
-      console.log("Partial update of the article.");
-      Article.findOneAndUpdate({ _id: id }, req.body.title)
-        .exec()
-        .then((article) => {
-          return res.status(200).json({
-            mensaje: "Data succesfully updated.",
-            status: "success",
-            article,
-          });
-        })
-        .catch((err) => {
-          return res.status(404).json({
-            status: "error",
-            mensaje: "An error ocurred with the updating proccess.",
-          });
+  } else if (!validatedTitle && validatedBody) {
+    Article.findOneAndUpdate(
+      { _id: id },
+      { $set: { title: req.body.title } },
+      { new: true }
+    )
+      .exec()
+      .then((article) => {
+        return res.status(200).json({
+          mensaje: "Data succesfully updated.",
+          status: "success",
+          article,
         });
-    } else if (!validatedBody) {
-    }
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          status: "error",
+          mensaje: "An error ocurred with the updating proccess.",
+        });
+      });
+  } else if (!validatedBody && validatedTitle) {
+    Article.findOneAndUpdate(
+      { _id: id },
+      { $set: { body: req.body.body } },
+      { new: true }
+    )
+      .exec()
+      .then((article) => {
+        return res.status(200).json({
+          mensaje: "Data succesfully updated.",
+          status: "success",
+          article,
+        });
+      })
+      .catch((error) => {
+        return res.status(500).json({
+          status: "error",
+          mensaje: "An error ocurred with the updating proccess.",
+        });
+      });
   }
+};
+
+const upload = (req, res) => {
+  if (!req.file && !req.files) {
+    return res.status(404).json({
+      status: "Error",
+      message: "Non-existent files",
+    });
+  }
+
+  let nameFile = req.file.originalname;
+  let fileSplit = nameFile.split(".");
+  let fileExtension = fileSplit[1];
+
+  if (
+    fileExtension != "png" &&
+    fileExtension != "jpg" &&
+    fileExtension != "jpeg" &&
+    fileExtension != "gif"
+  ) {
+    fs.unlink(req.file.path, (err) => {
+      return res.status(404).json({
+        status: "Error",
+        message: "Invalid file",
+      });
+    });
+  } else {
+    let id = req.params.id;
+
+    Article.findOneAndUpdate(
+      { _id: id },
+      { $set: { image: req.file.filename } },
+      { new: true }
+    )
+      .exec()
+      .then((article) => {
+        return res.status(200).json({
+          mensaje: "Image succesfully updated.",
+          status: "success",
+          article,
+        });
+      })
+      .catch((error) => {
+        return res.status(500).json({
+          status: "Error",
+          mensaje: "An error ocurred with the updating proccess.",
+        });
+      });
+  }
+};
+
+const getImage = (req, res) => {
+  let fileName = req.params.filePath;
+  let filePath = "./images/article/" + fileName;
+  fs.stat(filePath, (error, exists) => {
+    if (exists) {
+      return res.sendFile(path.resolve(filePath));
+    } else {
+      return res.status(404).json({
+        status: "Error",
+        mensaje: "The file does not exist.",
+      });
+    }
+  });
+};
+
+const searcher = (req, res) => {
+  let keyWord = req.body.keyWord;
+
+  Article.find({
+    $or: [
+      { title: { $regex: keyWord, $options: "i" } },
+      { body: { $regex: keyWord, $options: "i" } },
+    ],
+  })
+    .sort({ date: -1 })
+    .exec()
+    .then((articles) => {
+      if (articles.length > 0) {
+        return res.status(200).json({
+          mensaje: "Data succesfully retrieved.",
+          status: "success",
+          articles,
+        });
+      } else {
+        return res.status(404).json({
+          mensaje: "No data found to retrieve.",
+          status: "Error",
+        });
+      }
+    })
+    .catch((error) => {
+      return res.status(404).json({
+        status: "error",
+        mensaje: "There's no data related to the search.",
+      });
+    });
 };
 
 //Exportamos todos los métodos
@@ -179,4 +307,7 @@ module.exports = {
   findArticle,
   deleteArticle,
   updateArticle,
+  upload,
+  getImage,
+  searcher,
 };
--- routes/article.js
+++ routes/article.js
@@ -1,14 +1,30 @@
 const { Router } = require("express");
 const router = Router();
-
 const ArticleController = require("../controllers/article");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./images/article/");
+  },
 
-//esto es una petición individual
+  filename: function (req, file, cb) {
+    cb(null, "article" + Date.now() + file.originalname);
+  },
+});
+const uploads = multer({ storage: storage });
+
 router.get("/ruta-de-prueba", ArticleController.pruebaPrimaria);
 router.post("/new-article", ArticleController.createArticle);
 router.get("/get-all-articles/:quantity?", ArticleController.retrieveAll);
 router.get("/get-article/:id", ArticleController.findArticle);
 router.delete("/delete-article/:id", ArticleController.deleteArticle);
 router.put("/update-article/:id", ArticleController.updateArticle);
+router.get(
+  "/upload-image/:id",
+  [uploads.single("file0")],
+  ArticleController.upload
+);
+router.get("/get-image/:filePath", ArticleController.getImage);
+router.get("/search-for", ArticleController.searcher);
 
 module.exports = router;
